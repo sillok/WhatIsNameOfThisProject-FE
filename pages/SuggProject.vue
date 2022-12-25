4a45a008a0f87051a247762fe2e3d5c1d53f5f48
김어진 <kimuj5090@gmail.com>
2022-12-25 09:05:34 +0900

    api 수정

--- pages/SuggProject.vue
+++ pages/SuggProject.vue
@@ -65,32 +65,20 @@ export default {
         this.loading = false
         return
       }
-      axios.post('http://172.30.1.85:8000/name/', {
-        item: this.item
+      axios.post('http://172.30.1.85:8000/fastname', {
+        item: this.item,
+      }, {
+        timeout: 5000,
       })
       .then(function (response) {
-        const key = response.data.key
-        const interval = setInterval(() => {
-          axios.get('http://172.30.1.85:8000/answer/', {
-            params: {
-              gpt_key: key
-            }
-          })
-          .then(function (response) {
-            it.response = true
-            it.loading = false
-            clearInterval(interval)
-            it.result = response.data.answer
-          })
-          .catch(function (error) {})
-        }, 2000);
+        it.response = true
+        it.loading = false
+        it.result = response.data.answer
       })
       .catch(function (error) {
-        alert(error)
-      })
-      .finally(function () {
         it.response = false
         it.loading = false
+        alert(error.data.detail)
       })
     }
   }
